2d10c4e55fd0db710fc96d95f390c824f625e283
Trozza <troypoulter@gmail.com>
2023-07-15 10:39:14 +0000

    test: add core test to verify file generates as expected

---
+++ cmd/entmaid_test.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+type testCase struct {
+	schemaPath     string
+	targetPath     string
+	expectedOutput string
+	startPattern   string
+	endPattern     string
+}
+
+const (
+	defaultStartPattern = "<!-- #start:entmaid -->"
+	defaultEndPattern   = "<!-- #end:entmaid -->"
+)
+
+func TestGenerateDiagram(t *testing.T) {
+	testCases := []testCase{
+		{
+			schemaPath:     "../examples/basic/schema",
+			targetPath:     "../examples/basic/erd.md",
+			expectedOutput: "../examples/basic/erd-expected.md",
+			startPattern:   defaultStartPattern,
+			endPattern:     defaultEndPattern,
+		},
+		{
+			schemaPath:     "../examples/basic_two/schema",
+			targetPath:     "../examples/basic_two/erd.md",
+			expectedOutput: "../examples/basic_two/erd-expected.md",
+			startPattern:   defaultStartPattern,
+			endPattern:     defaultEndPattern,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(fmt.Sprintf("SchemaPath=%s, TargetPath=%s", tc.schemaPath, tc.targetPath), func(t *testing.T) {
+			// Generate the diagram
+			err := GenerateDiagram(tc.schemaPath, tc.targetPath, Markdown, tc.startPattern, tc.endPattern)
+			if err != nil {
+				t.Fatalf("Failed to generate diagram: %v", err)
+			}
+
+			// Compare the generated file with the expected output file
+			if !compareFiles(tc.targetPath, tc.expectedOutput) {
+				t.Errorf("Generated file does not match the expected output")
+			}
+		})
+	}
+}
+
+func compareFiles(file1, file2 string) bool {
+	content1, err := os.ReadFile(file1)
+	if err != nil {
+		fmt.Printf("Failed to read file %s: %v\n", file1, err)
+		return false
+	}
+
+	content2, err := os.ReadFile(file2)
+	if err != nil {
+		fmt.Printf("Failed to read file %s: %v\n", file2, err)
+		return false
+	}
+
+	return string(content1) == string(content2)
+}
---
+++ examples/basic_two/erd.md
@@ -0,0 +1,26 @@
+# Example Basic - Generated Schema
+<!-- #start:entmaid -->
+```mermaid
+erDiagram
+	Car {
+		int id
+		string model
+		time-Time registered_at
+	}
+
+	Group {
+		int id
+		string name
+	}
+
+	User {
+		int id
+		int age
+		string name
+	}
+
+	Group }o--o{ User : users-groups
+	User |o--o{ Car : cars-owner
+
+```
+<!-- #end:entmaid -->
