01049fdcb73be45111d344f8446f4fcc31c1646d
Trozza <troypoulter@gmail.com>
2023-07-18 09:21:44 +0000

    tests: add edgeschema example

--- Makefile
+++ Makefile
@@ -11,7 +11,10 @@ example.start:
 example.m2m2types:
 	go run main.go -s ./examples/m2m2types/schema -t ./examples/m2m2types/readme.md -o markdown
 
-example.all: example.readme example.start example.m2m2types
+example.edgeschema:
+	go run main.go -s ./examples/edgeschema/schema -t ./examples/edgeschema/readme.md -o markdown
+
+example.all: example.readme example.start example.m2m2types example.edgeschema
 
 build:
 	go build -o ./bin/entmaid
---
+++ examples/edgeschema/schema/relationshipinfo.go
@@ -0,0 +1,25 @@
+// Copyright 2019-present Facebook Inc. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+//
+// Code has been copied from the ent examples folder to demonstrate common schema patterns.
+// You can find the original code here: https://github.com/ent/ent/tree/master/entc/integration/edgeschema
+
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// RelationshipInfo holds the schema definition for the RelationshipInfo entity.
+type RelationshipInfo struct {
+	ent.Schema
+}
+
+// Fields of the RelationshipInfo.
+func (RelationshipInfo) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("text"),
+	}
+}
